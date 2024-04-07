2b858cde5acdf7c1c4d7b2c2fa5dd5f2ce21369b
kite <2257440643@qq.com>
2024-04-07 12:18:55 +0800

    feat: move zero to end

--- src/practice/10.fibonacci.js
+++ src/practice/10.fibonacci.js
@@ -17,22 +17,22 @@
  * 斐波那契数列（循环）
  * @param n n
  */
-export function fibonacci(n) {
-  if (n <= 0) return 0;
+// 0 1 1 2 3 5 8 13 21
+function fibonacci(n) {
+  if (n === 0) return 0;
   if (n === 1) return 1;
 
-  let n1 = 1; // 记录 n-1 的结果
-  let n2 = 0; // 记录 n-2 的结果
   let res = 0;
+  let n1 = 0; // 记录 n-2 的结果
+  let n2 = 1; // 记录 n-1 的结果
 
   for (let i = 2; i <= n; i++) {
     res = n1 + n2;
 
-    n2 = n1;
-    n1 = res;
+    n1 = n2;
+    n2 = res;
   }
-
   return res;
 }
 
-// console.log(fibonacci(10))
+console.log(fibonacci(10));
---
+++ src/practice/11.move-zero.js
@@ -0,0 +1,83 @@
+/**
+ * @description 移动 0 到数组末尾,在原数组操作
+ */
+
+/**
+ * 移动 0 到数组的末尾（嵌套循环）
+ * @param arr number arr
+ */
+export function moveZero1(arr) {
+  const length = arr.length;
+  if (length === 0) return;
+
+  let zeroLength = 0;
+
+  // O(n^2)
+  for (let i = 0; i < length - zeroLength; i++) {
+    if (arr[i] === 0) {
+      arr.push(0);
+      arr.splice(i, 1); // 本身就有 O(n)
+      i--; // 数组截取了一个元素，i 要递减，否则连续 0 就会有错误
+      zeroLength++; // 累加 0 的长度
+    }
+  }
+}
+
+/**
+ * 移动 0 到数组末尾（双指针）
+ * @param arr number arr
+ */
+export function moveZero2(arr) {
+  const length = arr.length;
+  if (length === 0) return;
+
+  let i;
+  let j = -1; // 指向第一个 0
+
+  for (i = 0; i < length; i++) {
+    if (arr[i] === 0) {
+      // 第一个 0
+      if (j < 0) {
+        j = i;
+      }
+    }
+    // 第 i 个元素不为 0 且 第 j 个 元素等于 0， 交换 arr[i], arr[j]
+    if (arr[i] !== 0 && j >= 0) {
+      // 交换
+      const n = arr[i];
+      arr[i] = arr[j];
+      arr[j] = n;
+
+      j++;
+    }
+  }
+}
+
+// 功能测试
+const arr = [0, 0, 3, 4, 0, 0, 11, 0];
+moveZero2(arr);
+console.log(arr);
+
+// const arr1 = [];
+// for (let i = 0; i < 20 * 10000; i++) {
+//   if (i % 10 === 0) {
+//     arr1.push(0);
+//   } else {
+//     arr1.push(i);
+//   }
+// }
+// console.time("moveZero1");
+// moveZero1(arr1);
+// console.timeEnd("moveZero1"); // 528.006ms
+
+// const arr2 = [];
+// for (let i = 0; i < 20 * 10000; i++) {
+//   if (i % 10 === 0) {
+//     arr2.push(0);
+//   } else {
+//     arr2.push(i);
+//   }
+// }
+// console.time("moveZero2");
+// moveZero2(arr2);
+// console.timeEnd("moveZero2"); // 3.03ms
